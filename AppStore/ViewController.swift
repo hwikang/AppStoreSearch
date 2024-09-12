7d78ac14e55306af9dc7d91f0be6beea4f132228
강휘 <kh@paytalab.com>
2024-09-12 18:00:33 +0900

    init

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by paytalab on 9/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
